9ae3eff6d296130d459061dc2b106980b34661bf
nisnislevi <nissan@certora.com>
2023-11-16 10:48:28 +0200

    voting chain mutant 5

--- src/contracts/GovernancePowerStrategy.sol
+++ src/contracts/GovernancePowerStrategy.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: BUSL-1.1
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import {IGovernancePowerDelegationToken} from 'aave-token-v3/interfaces/IGovernancePowerDelegationToken.sol';
@@ -51,7 +51,7 @@ contract GovernancePowerStrategy is
 
     address[] memory votingAssetList = getVotingAssetList();
     for (uint256 i = 0; i < votingAssetList.length; i++) {
-      fullGovernancePower += IGovernancePowerDelegationToken(votingAssetList[i])
+      fullGovernancePower += IGovernancePowerDelegationToken(votingAssetList[0])
         .getPowerCurrent(user, powerType);
     }
 
